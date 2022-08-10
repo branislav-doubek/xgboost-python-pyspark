c01fc9480a06e7e20ecbcdca5ffa03c20f3319d3
Branislav Doubek <doubekbranislav@gmail.com>
2022-08-10 23:45:57 +0200

    fixed indentation

--- pyspark_xgb/train_binary.py
+++ pyspark_xgb/train_binary.py
@@ -53,58 +53,58 @@ def main():
         feature_cols = [col for col in train.columns if col not in safe_cols]
         label_col = 'label'
 
-    def objective(trial):
-        max_depth = trial.suggest_int('max_depth', 5, 30)
-        eta = trial.suggest_loguniform('eta', 0.001, 0.01)
-        gamma = trial.suggest_float('gamma', 1, 30)
-        subsample = trial.suggest_float('subsample', 0.01, 0.6)
-        min_child_weight = trial.suggest_float('min_child_weight', 1, 50)
-        colsample_bytree = trial.suggest_float('colsample_bytree', 0.3, 1)
+        def objective(trial):
+            max_depth = trial.suggest_int('max_depth', 5, 30)
+            eta = trial.suggest_loguniform('eta', 0.001, 0.01)
+            gamma = trial.suggest_float('gamma', 1, 30)
+            subsample = trial.suggest_float('subsample', 0.01, 0.6)
+            min_child_weight = trial.suggest_float('min_child_weight', 1, 50)
+            colsample_bytree = trial.suggest_float('colsample_bytree', 0.3, 1)
+            params = DevXGBoostModelHyperparams()
+            params.eval_metric = config['modeling']['eval_metric']
+            params.objective = config['modeling']['objective']
+            params.max_depth = max_depth
+            params.eta = eta
+            params.gamma = gamma
+            params.subsample = subsample
+            params.min_child_weight = min_child_weight
+            params.colsample_bytree = colsample_bytree
+            logging.info('parameters')
+            logging.info(params)
+            model1 = DevXGBoostModel(params, XGBoostEstimator, feature_cols, label_col, actions=[])
+            model2 = DevXGBoostModel(params, XGBoostEstimator, feature_cols, label_col, actions=[])
+            train1 = train.filter(f.col('CD_PERIOD') < 202203)
+            train2 = train.filter(f.col('CD_PERIOD') >= 202203)
+            valid1 = valid.filter(f.col('CD_PERIOD') < 202203)
+            valid2 = valid.filter(f.col('CD_PERIOD') >= 202203)
+            score1 = model1.cross_validate(train1, valid1)
+            score2 = model2.cross_validate(train2, valid2)
+            score = (score1+score2) / 2
+            return score
+    
+        study = optuna.create_study(direction='maximize')
+        study.optimize(objective, n_trials=2)
+
         params = DevXGBoostModelHyperparams()
-        params.eval_metric = config['modeling']['eval_metric']
-        params.objective = config['modeling']['objective']
-        params.max_depth = max_depth
-        params.eta = eta
-        params.gamma = gamma
-        params.subsample = subsample
-        params.min_child_weight = min_child_weight
-        params.colsample_bytree = colsample_bytree
-        logging.info('parameters')
-        logging.info(params)
-        model1 = DevXGBoostModel(params, XGBoostEstimator, feature_cols, label_col, actions=[])
-        model2 = DevXGBoostModel(params, XGBoostEstimator, feature_cols, label_col, actions=[])
-        train1 = train.filter(f.col('CD_PERIOD') < 202203)
-        train2 = train.filter(f.col('CD_PERIOD') >= 202203)
-        valid1 = valid.filter(f.col('CD_PERIOD') < 202203)
-        valid2 = valid.filter(f.col('CD_PERIOD') >= 202203)
-        score1 = model1.cross_validate(train1, valid1)
-        score2 = model2.cross_validate(train2, valid2)
-        score = (score1+score2) / 2
-        return score
-    
-    study = optuna.create_study(direction='maximize')
-    study.optimize(objective, n_trials=2)
+        params.colsample_bytree = best_params['colsample_bytree']
+        params.eta = best_params['eta']
+        params.gamma = best_params['gamma']
+        params.max_depth = best_params['max_depth']
+        params.min_child_weight = best_params['min_child_weight']
+        params.subsample = best_params['subsample']
 
-    params = DevXGBoostModelHyperparams()
-    params.colsample_bytree = best_params['colsample_bytree']
-    params.eta = best_params['eta']
-    params.gamma = best_params['gamma']
-    params.max_depth = best_params['max_depth']
-    params.min_child_weight = best_params['min_child_weight']
-    params.subsample = best_params['subsample']
+        print('Best parameters of the model:')
+        print(params) 
 
-    print('Best parameters of the model:')
-    print(params) 
-
-    model = DevXGBoostModel(params, XGBoostEstimator, feature_cols, label_col, actions=[])
-    score = model.cross_validate(train, valid)
+        model = DevXGBoostModel(params, XGBoostEstimator, feature_cols, label_col, actions=[])
+        score = model.cross_validate(train, valid)
 
 
-    from pyspark.mllib.evaluation import MulticlassMetrics
-    predictions = model.predict(test)
-    predictions_labels = predictions.rdd.map(lambda x: (x['prediction'], x['label']))
-    metrics = MulticlassMetrics(predictions_labels)
-    
+        from pyspark.mllib.evaluation import MulticlassMetrics
+        predictions = model.predict(test)
+        predictions_labels = predictions.rdd.map(lambda x: (x['prediction'], x['label']))
+        metrics = MulticlassMetrics(predictions_labels)
+
     except Exception:
         logger.error(traceback.print_exc())
 
