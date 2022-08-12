bfd2d13b926a70521f49a4423312ca274be386da
Branislav Doubek <doubekbranislav@gmail.com>
2022-08-12 11:38:20 +0200

    f

--- pyspark_xgb/train_binary_class.py
+++ pyspark_xgb/train_binary_class.py
@@ -118,7 +118,7 @@ def main():
         predictions_labels = pred.rdd.map(
                     lambda x: (x['prediction'], x['LABEL']))
         metrics = MulticlassMetrics(predictions_labels)
-        labels = pred.rdd.map(lambda lp: lp.LABEL).distinct().collect()
+        labels = pred.rdd.map(lambda lp: float(lp.LABEL)).distinct().collect()
         score = 0
         for label in sorted(labels[1:]):
             print(
