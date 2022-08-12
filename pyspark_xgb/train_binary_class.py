60f6a222d9a49884ac7ab726caba613f4efd1980
Branislav Doubek <doubekbranislav@gmail.com>
2022-08-12 09:51:03 +0200

    a

--- pyspark_xgb/train_binary_class.py
+++ pyspark_xgb/train_binary_class.py
@@ -77,7 +77,7 @@ def main():
         features = [c for c in train.columns if c not in  safe_cols]
 
         assembler = VectorAssembler(inputCols=features, outputCol=FEATURES)
-        train, weights = weight_mapping(train_ds, label_col)
+        train, weights = weight_mapping(train, label_col)
         valid = weight_mapping(valid, label_col, weights)
 
         train = assembler.transform(train).select(FEATURES, LABEL, WEIGHT)
