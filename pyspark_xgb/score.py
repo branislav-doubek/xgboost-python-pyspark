8f81a2760e53a135d6594ccccd9d0719604f808c
Branislav Doubek <doubekbranislav@gmail.com>
2022-10-21 18:44:31 +0200

    f

--- pyspark_xgb/score.py
+++ pyspark_xgb/score.py
@@ -3,7 +3,7 @@ import traceback
 import numpy as np
 from pyspark.ml.feature import VectorAssembler
 from spark import get_spark, get_logger
-from utils import load_config
+from utils import load_config, predict, load_model, 
 
 # assert len(os.environ.get('JAVA_HOME')) != 0, 'JAVA_HOME not set'
 assert len(os.environ.get('SPARK_HOME')) != 0, 'SPARK_HOME not set'
@@ -15,6 +15,7 @@ PARENT_PROJ_PATH = '/'.join(abspath.split(os.sep)[:-2])
 PYSPARK_PROJ_PATH = '/'.join(abspath.split(os.sep)[:-1])
 CONFIG_PATH = '/home/config.yml'
 MODEL_PATH = '/output'
+DATASET_PATH = '/home/dataset'
 LOCAL_MODEL_PATH = MODEL_PATH
 
 
@@ -25,22 +26,18 @@ def main():
         # init spark
         spark = get_spark(app_name="pyspark-xgb")
 
-        # get logger
-        logger = get_logger(spark, "app")
-
         # load data
         score = spark.read.parquet(DATASET_PATH + '/score')
 
         # preprocess
         LABEL = 'LABEL'
         FEATURES = 'features'
-        WEIGHT = 'weight'
 
         safe_cols = [
             'ID_CUSTOMER',
             'LABEL',
             'CD_PERIOD']
-        features = [c for c in train.columns if c not in  safe_cols]
+        features = [c for c in score.columns if c not in  safe_cols]
 
         assembler = VectorAssembler(inputCols=features, outputCol=FEATURES)
 
@@ -50,10 +47,7 @@ def main():
         xgb_cls_model = load_model(local_model_path)
         pred = predict(xgb_cls_model, score)
 
-        pred.select(safe_cols + 'probability').write.parquet()
-        slogloss = pred.withColumn('log_loss', udf_logloss(LABEL, 'probability')) \
-            .agg({"log_loss": "mean"}).collect()[0]['avg(log_loss)']
-        print('[xgboost] valid logloss: {}'.format(slogloss))
+        pred.select(safe_cols + 'probability').write.parquet("/data_output/prediction")
 
     except Exception:
         print(traceback.print_exc())
